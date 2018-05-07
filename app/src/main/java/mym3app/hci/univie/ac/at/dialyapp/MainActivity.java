0f7972dfeabf248dee185a410ea0734e423328ea
Maurice Harfmann <mp.harfmann@gmail.com>
2018-05-07 23:09:15 +0200

    Rohversion der App

--- app/src/main/java/mym3app/hci/univie/ac/at/dialyapp/MainActivity.java
+++ app/src/main/java/mym3app/hci/univie/ac/at/dialyapp/MainActivity.java
@@ -1,5 +1,6 @@
 package mym3app.hci.univie.ac.at.dialyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,23 +11,59 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.support.v7.widget.GridLayout;
+//import android.support.v7.widget.GridLayout;
 import android.support.v7.widget.CardView;
-//import android.support.v7.widget.LinearLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    //public static final String EXTRA_MESSAGE = "mymm3app.hci.univie.ac.at.MESSAGE";
 
-    GridLayout mainGrid;
+    LinearLayout mainLayout;
+    CardView highlights_btn;
+    CardView stats_btn;
+    CardView new_entry_btn;
     public static int theme = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
+        highlights_btn = (CardView) findViewById(R.id.highlights_btn);
+        stats_btn = (CardView) findViewById(R.id.stats_btn);
+        new_entry_btn = (CardView) findViewById(R.id.new_entry_btn);
+
+        highlights_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Highlights.class);
+                startActivity(intent);
+            }
+        });
+
+        stats_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Statistics.class);
+                startActivity(intent);
+            }
+        });
+
+        new_entry_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewEntry_1.class);
+                startActivity(intent);
+            }
+        });
+
+
         switch (theme) {
             case 0:
 
@@ -45,8 +82,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
+        /*gridview.setAdapter(new ImageAdapter(this));
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
@@ -63,22 +99,27 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
-        //setSingleEvent(mainGrid);
     }
 
-    /*private void setSingleEvent(GridLayout mainGrid) {
-        for (int i = 0; i < mainGrid.getChildCount(); i++) {
-            CardView cardView = (CardView) mainGrid.getChildAt(i);
+    public void sendMessage(View view) {
+        Toast.makeText(MainActivity.this, "Clicked on item.", Toast.LENGTH_LONG).show();
+    }
+
+    /*
+    private void setSingleEvent(LinearLayout mainLayout) {
+        for (int i = 0; i < mainLayout.getChildCount(); i++) {
+            CardView cardView = (CardView) mainLayout.getChildAt(i);
             final int fi = i;
             cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(MainActivity.this, "Clicked on item"+fi, Toast.LENGTH_LONG).show();
+                    //Toast.makeText(MainActivity.this, "Clicked on item"+fi, Toast.LENGTH_LONG).show();
+                    sendMessage([this, fi);
                 }
             });
         }
-    }*/
+    }
+    */
 
     @Override
     public void onBackPressed() {
---
+++ app/src/main/java/mym3app/hci/univie/ac/at/dialyapp/NewEntry_1.java
@@ -0,0 +1,32 @@
+package mym3app.hci.univie.ac.at.dialyapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class NewEntry_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_entry_1);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NewEntry_1.this, NewEntry_2.class);
+                startActivity(intent);
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
