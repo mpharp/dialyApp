e83bfbd6ae7295bcd48fa8b2db25822fc5b868c5
Maurice Harfmann <mp.harfmann@gmail.com>
2018-05-10 19:45:23 +0200

    New Entry Screen 1, ohne Daten bei Back beizubehalten

--- app/src/main/java/mym3app/hci/univie/ac/at/dialyapp/NewEntry_1.java
+++ app/src/main/java/mym3app/hci/univie/ac/at/dialyapp/NewEntry_1.java
@@ -1,14 +1,46 @@
 package mym3app.hci.univie.ac.at.dialyapp;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Calendar;
 
 public class NewEntry_1 extends AppCompatActivity {
+
+    public static final String TITLE = "title";
+    public static final String DATE = "date";
+    public static final String CATEGORY = "category";
+    public static final String LOCATION ="location";
+
+    EditText new_entry_title;
+    TextView new_entry_date_view;
+    Button new_entry_date_btn;
+
+    CardView cat_work;
+    CardView cat_friends;
+    CardView cat_food;
+    CardView cat_shopping;
+    CardView cat_health;
+    CardView cat_custom;
+
+    Calendar cal;
+    DatePickerDialog date_pick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +49,141 @@ public class NewEntry_1 extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        new_entry_title = (EditText) findViewById(R.id.new_entry_title);
+
+
+        new_entry_date_view = (TextView) findViewById(R.id.new_entry_date_view);
+        new_entry_date_btn = (Button) findViewById(R.id.new_entry_date_btn);
+
+        new_entry_date_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cal = Calendar.getInstance();
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+
+                date_pick = new DatePickerDialog(NewEntry_1.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int yr, int mnth, int dy) {
+                        new_entry_date_view.setText(dy + ". " + (mnth+1) + ". " + yr);
+                    }
+                }, year, month, day);
+                date_pick.show();
+            }
+        });
+
+        final boolean[] press_arr = new boolean[6];
+        Arrays.fill(press_arr, Boolean.FALSE);
+
+        cat_work = (CardView) findViewById(R.id.cat_work);
+        cat_work.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (press_arr[0]) {
+                    cat_work.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                    cat_work.setCardElevation(2);
+                    press_arr[0] = false;
+                } else {
+                    cat_work.setCardBackgroundColor(Color.parseColor("#333333"));
+                    cat_work.setCardElevation(8);
+                    press_arr[0] = true;
+                }
+            }
+        });
+
+        cat_friends = (CardView) findViewById(R.id.cat_friends);
+        cat_friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (press_arr[1]) {
+                    cat_friends.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                    cat_friends.setCardElevation(2);
+                    press_arr[1] = false;
+                } else {
+                    cat_friends.setCardBackgroundColor(Color.parseColor("#333333"));
+                    cat_friends.setCardElevation(8);
+                    press_arr[1] = true;
+                }
+            }
+        });
+
+        cat_food = (CardView) findViewById(R.id.cat_food);
+        cat_food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (press_arr[2]) {
+                    cat_food.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                    cat_food.setCardElevation(2);
+                    press_arr[2] = false;
+                } else {
+                    cat_food.setCardBackgroundColor(Color.parseColor("#333333"));
+                    cat_food.setCardElevation(8);
+                    press_arr[2] = true;
+                }
+            }
+        });
+
+        cat_shopping = (CardView) findViewById(R.id.cat_shopping);
+        cat_shopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (press_arr[3]) {
+                    cat_shopping.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                    cat_shopping.setCardElevation(2);
+                    press_arr[3] = false;
+                } else {
+                    cat_shopping.setCardBackgroundColor(Color.parseColor("#333333"));
+                    cat_shopping.setCardElevation(8);
+                    press_arr[3] = true;
+                }
+            }
+        });
+
+        cat_health = (CardView) findViewById(R.id.cat_health);
+        cat_health.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (press_arr[4]) {
+                    cat_health.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                    cat_health.setCardElevation(2);
+                    press_arr[4] = false;
+                } else {
+                    cat_health.setCardBackgroundColor(Color.parseColor("#333333"));
+                    cat_health.setCardElevation(8);
+                    press_arr[4] = true;
+                }
+            }
+        });
+
+        cat_custom = (CardView) findViewById(R.id.cat_custom);
+        cat_custom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (press_arr[5]) {
+                    cat_custom.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                    cat_custom.setCardElevation(2);
+                    press_arr[5] = false;
+                } else {
+                    cat_custom.setCardBackgroundColor(Color.parseColor("#333333"));
+                    cat_custom.setCardElevation(8);
+                    press_arr[5] = true;
+                }
+            }
+        });
+
+        final String cat = String.valueOf(press_arr.length);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(NewEntry_1.this, NewEntry_2.class);
+                intent.putExtra(TITLE, new_entry_title.getText().toString());
+                intent.putExtra(DATE, new_entry_date_view.getText());
+                intent.putExtra(CATEGORY, press_arr);
+                intent.putExtra(LOCATION, "");
+
                 startActivity(intent);
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 //        .setAction("Action", null).show();
--- app/src/main/java/mym3app/hci/univie/ac/at/dialyapp/NewEntry_2.java
+++ app/src/main/java/mym3app/hci/univie/ac/at/dialyapp/NewEntry_2.java
@@ -7,8 +7,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class NewEntry_2 extends AppCompatActivity {
+
+    TextView txt1;
+    TextView txt2;
+    TextView txt3;
+    TextView txt4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,31 @@ public class NewEntry_2 extends AppCompatActivity {
         setContentView(R.layout.activity_new_entry_2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+
+        String title = intent.getStringExtra(NewEntry_1.TITLE);
+        String date = intent.getStringExtra(NewEntry_1.DATE);
+        boolean[] categories = intent.getBooleanArrayExtra(NewEntry_1.CATEGORY);
+        String location = intent.getStringExtra(NewEntry_1.LOCATION);
+
+        String cat = String.valueOf(categories.length) + ": ";
+
+        for (boolean press: categories){
+            if (press) cat += "1";
+            else cat += "0";
+        }
+
+        txt1 = (TextView) findViewById(R.id.txt1);
+        txt2 = (TextView) findViewById(R.id.txt2);
+        txt3 = (TextView) findViewById(R.id.txt3);
+        txt4 = (TextView) findViewById(R.id.txt4);
+
+        txt1.setText(title);
+        txt2.setText(date);
+        txt3.setText(cat);
+        txt4.setText(location);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
